d9ce715de9d58bbb04561c92c89bfd8c7658bf93
Abednego Ayienda <davenanony@gmail.com>
2026-01-03 14:58:45 +0300

    Import additional Firestore functions and create collection

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getFirestore, collection, addDoc, onSnapshot, deleteDoc, doc, query, orderBy } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyDcpr6pCVBBauYeQeGllPypgojJ7v4E_f8",
@@ -13,3 +13,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
+export const assignmentsCollection = collection(db, 'assignments');
